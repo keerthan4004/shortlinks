0be26db933768c5c6ed2c9e7b9a79e38350f2dcf
keerthan <keerthan357@gmail.com>
2026-03-26 01:03:25 +0530

    models

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+import random
+import string
 # Create your models here.
+def shortcode():
+    characters = string.ascii_letters + string.digits
+    while True:
+        code = ''.join(random.choices(characters, k=6))
+        if not URL.objects.filter(short_code=code).exists():
+            return code
+        
+class URL(models.Model):
+    original = models.URLField(max_length=2000)
+    short = models.CharField(max_length=10, unique=True, default=shortcode)
+    created_at = models.DateTimeField(auto_now_add=True)
+    click_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.short} to {self.original}"
