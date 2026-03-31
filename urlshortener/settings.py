58baee847a21a8c9ea8d4305b3cdac3d7f9f22a5
keerthan <keerthan357@gmail.com>
2026-03-31 11:01:05 +0530

    vercel

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-v-h1n+ac+u2vjec2fl#z$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,.vercel.app').split(',')
 
 
 # Application definition
